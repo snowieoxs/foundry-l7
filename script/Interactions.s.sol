adf2040e4adeb87b93419aa39418baae879c6882
snowieoxs <snowieoxs@gmail.com>
2024-04-17 09:22:03 -0500

    before making changes found in discussion everything seems to work as is, but something isn't right

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
 
+// Fund
+// Withdraw
+
 pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
@@ -7,12 +10,12 @@ import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
-    uint256 constant SEND_VALUE = 0.01 ether;
+    uint256 constant SEND_VALUE = 0.1 ether;
 
     function fundFundMe(address mostRecentlyDeployed) public {
-        vm.startBroadcast();
+        vm.startBroadcast(); // removed @ 11.34
         FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
-        vm.stopBroadcast();
+        vm.stopBroadcast(); // remove @ 11.34
         console.log("Funded FundMe with %s", SEND_VALUE);
     }
 
@@ -21,8 +24,28 @@ contract FundFundMe is Script {
             "FundMe",
             block.chainid
         );
+        // vm.startBroadcast(); // moved @ 11.34
         fundFundMe(mostRecentlyDeployed);
+        // vm.stopBroadcast(); // moved @ 11.34
     }
 }
 
-contract WithdrawFundMe is Script {}
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast(); // added @ 13.26
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast(); // added @ 13.26
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        // vm.startBroadcast(); // removed @ 13.27
+        withdrawFundMe(mostRecentlyDeployed);
+        // vm.stopBroadcast(); // removed @ 13.27
+    }
+}
+
+// 13.06
